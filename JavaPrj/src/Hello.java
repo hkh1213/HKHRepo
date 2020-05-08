4e1d724896394d5bc9a5a01ff557e505fe815528
DATA8320-03 <DATA8320-03@DESKTOP-8V4O9J1>
2020-05-08 11:16:35 +0900

    신규 프로젝트 올립니다.

---
+++ JavaPrj/src/Hello.java
@@ -0,0 +1,66 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+System.out.println("Hello, it's me\r\n" + 
+		"I was wondering if after all these years you'd like to meet\r\n" + 
+		"To go over everything\r\n" + 
+		"They say that time's supposed to heal ya\r\n" + 
+		"But I ain't done much healing\r\n" + 
+		"Hello, can you hear me?\r\n" + 
+		"I'm in California dreaming about who we used to be\r\n" + 
+		"When we were younger and free\r\n" + 
+		"I've forgotten how it felt before the world fell at our feet\r\n" + 
+		"There's such a difference between us\r\n" + 
+		"And a million miles\r\n" + 
+		"Hello from the other side\r\n" + 
+		"I must've called a thousand times\r\n" + 
+		"To tell you I'm sorry\r\n" + 
+		"For everything that I've done\r\n" + 
+		"But when I call you never\r\n" + 
+		"Seem to be home\r\n" + 
+		"Hello from the outside\r\n" + 
+		"At least I can say that I've tried\r\n" + 
+		"To tell you I'm sorry\r\n" + 
+		"For breaking your heart\r\n" + 
+		"But it don't matter, it clearly\r\n" + 
+		"Doesn't tear you apart anymore\r\n" + 
+		"Hello, how are you?\r\n" + 
+		"It's so typical of me to talk about myself, I'm sorry\r\n" + 
+		"I hope that you're well\r\n" + 
+		"Did you ever make it out of that town\r\n" + 
+		"Where nothing ever happened?\r\n" + 
+		"It's no secret\r\n" + 
+		"That the both of us\r\n" + 
+		"Are running out of time\r\n" + 
+		"So hello from the other side (other side)\r\n" + 
+		"I must've called a thousand times (thousand times)\r\n" + 
+		"To tell you I'm sorry\r\n" + 
+		"For everything that I've done\r\n" + 
+		"But when I call you never\r\n" + 
+		"Seem to be home\r\n" + 
+		"Hello from the outside (outside)\r\n" + 
+		"At least I can say that I've tried (I've tried)\r\n" + 
+		"To tell you I'm sorry\r\n" + 
+		"For breaking your heart\r\n" + 
+		"But it don't matter, it clearly\r\n" + 
+		"Doesn't tear you apart anymore\r\n" + 
+		"Ooooohh, anymore\r\n" + 
+		"Ooooohh, anymore\r\n" + 
+		"Ooooohh, anymore\r\n" + 
+		"Anymore\r\n" + 
+		"Hello from the other side (other side)\r\n" + 
+		"I must've called a thousand times (thousand times)\r\n" + 
+		"To tell you I'm sorry\r\n" + 
+		"For everything that I've done\r\n" + 
+		"But when I call you never\r\n" + 
+		"Seem to be home\r\n" + 
+		"Hello from the outside (outside)\r\n" + 
+		"At least I can say that I've tried (I've tried)\r\n" + 
+		"To tell you I'm sorry\r\n" + 
+		"For breaking your heart\r\n" + 
+		"But it don't matter, it clearly\r\n" + 
+		"Doesn't tear you apart anymore");
+	}
+
+}
