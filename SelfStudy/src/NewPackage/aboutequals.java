f63a055a0e41f1be0597e9d0f01ea4d616027ac8
kopo <kopo@DESKTOP-QPH94A3>
2020-05-19 11:54:53 +0900

    why?

--- SelfStudy/src/NewPackage/aboutequals.java
+++ SelfStudy/src/NewPackage/aboutequals.java
@@ -2,4 +2,22 @@ package NewPackage;
 
 public class aboutequals {
 
+	public static void main(String[] args) {
+		String answer="";
+	String[] seoul = {"jung","eun","fuck","kim"};
+	int a=0;
+	for(int i=0;i<seoul.length;i++) {
+		if(seoul[i].equals("kim")) {
+			a=i;
+		} //배열값을 같다고해줄때 배열은 객체이므로 equals.()를 사용해야함
+		
+	}
+
+	StringBuilder sb =new StringBuilder();
+	   sb.append("김서방은 ");
+	   sb.append(a);
+	   sb.append("에 있다");
+	   answer=sb.toString();
+	System.out.println(answer);
+	}
 }
