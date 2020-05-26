6378d585d066ef57c47a0acf53b0eb8a150e82c4
kopo <kopo@DESKTOP-QPH94A3>
2020-05-26 15:18:43 +0900

    알고리즘2

---
+++ SelfStudy/src/NewPackage/Algorithm2.java
@@ -0,0 +1,22 @@
+package NewPackage;
+
+import java.util.Scanner;
+
+public class Algorithm2 {
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		int x,n;
+		x=scn.nextInt();
+		n=scn.nextInt();
+		int result=factorial(x,n);
+		System.out.println(result);
+	}
+
+	public static int factorial(int a, int b) {
+		if(b<=0)
+			return 1;
+		else
+		return b*factorial(a,b-1);
+	}
+
+}
