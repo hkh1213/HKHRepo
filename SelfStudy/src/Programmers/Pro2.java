a1e20026abb4bfc3091fcc8df50a9a0abd6946de
henry <henry_hong@naver.com>
2020-06-01 13:37:15 +0900

    잔디1일차

---
+++ SelfStudy/src/Programmers/Pro2.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+public class Pro2 {
+
+	public static void main(String[] args) {
+
+	}
+	public static int findMin(int[] answer,int st,int end) {
+		int min;
+		if(st==end) {
+			min= answer[st];
+		}
+		else 
+			min= Math.min(answer[st], findMin(answer, st+1,end));
+		
+		return min;
+	}
+
+}
