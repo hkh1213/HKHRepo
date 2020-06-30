5a845163c6555b5a9f3f33b54f2c040233acec8b
henry <henry_hong@naver.com>
2020-06-30 18:11:58 +0900

    11

--- SelfStudy/src/Programmers/Pro25.java
+++ SelfStudy/src/Programmers/Pro25.java
@@ -1,43 +1,38 @@
 package Programmers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Pro25 {
+	static StringBuilder sb=new StringBuilder();
 	public static void main(String[] args) {
-		int n=0;
-		int a=11;
-		int count=0;
-		System.out.println(sol(1));
-		System.out.println(sol(2));
-		while(sol(n)<a) {
-			n++;
-			count=n;
-		}
-		System.out.println(count);
-		List<Integer> list=new ArrayList<Integer>();
-		
-		while(count>0) {
-		if(a-sol(count-1)<(sol(count)-sol(count-1))/3)
-			list.add(1);
-		else if(a-sol(count-1)>=(sol(count)-sol(count-1))/3)
-			list.add(2);
-		else if(a-sol(count-1)>=(sol(count)-sol(count-1))/3*2)
-			list.add(4);
-		
-		count--;
-		System.out.println(a);
-		}
-		System.out.println(list);
-	}
-	public static double sol(int num) {
-		
-		if(num==0)
-			return 0;
-		else 
-			return sol(num-1)+Math.pow(3, num);
-		
-	}
-	
 
+	int n=18;
+	int m=n/3;
+System.out.println(tr(n));
+}
+	public static int tr(int n) {
+		int m=n/3;
+
+		if(m==0) {
+			System.out.println(sb.reverse());
+			return 0;
+		}
+			
+		if (n % 3 == 1) {
+			sb.append("1");
+		}
+		else if (n % 3 == 2) {
+			sb.append("2");
+		}
+		else if( n%3==0){
+			sb.append("4");
+		}
+		if(m>3) {
+		if(m%3==1)
+			sb.append("1");
+		else if(m%3==2||n%9==0)
+			sb.append("2");
+		else if(m%3==0&&n%9!=0)
+			sb.append("4");
+		}
+		return tr(m);
+	}
 }
--- SelfStudy/src/Programmers/Pro26.java
+++ SelfStudy/src/Programmers/Pro26.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Pro26 {
 
 	public static void main(String[] args) {
-		int[] numbers= {6,52,1,21,45,7,106,25};
+		int[] numbers= {6,52,1,21,45,7,106,25,226};
 		String answer="";
 		List<Integer> list=new ArrayList<Integer>();
 		String starr[]=Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
@@ -20,25 +20,32 @@ public class Pro26 {
 					starr[j]=s;
 				}
 				
-				
-				
 			}
-	}
-		for(int i=0;i<numbers.length-1;i++) {
-			for(int j=0;j<starr[i].length();j++) {
-				if(starr[i].charAt(j)==starr[i+1].charAt(j)) {
-					int a=Integer.parseInt(starr[i]);
-					int b=Integer.parseInt(starr[i+1]);
-					if(a<b) {
-						String s=starr[i];
-						starr[i]=starr[i+1];
-						starr[i+1]=s;
-					}
+	}for(int i=0;i<numbers.length-1;i++) {
+		for(int j=0;j<Math.min(starr[i].length(),starr[i+1].length());j++) {
+			if(starr[i].charAt(j)==starr[i+1].charAt(j)) {
+				String s=starr[i];
+				starr[i]=starr[i+1];
+				starr[i+1]=s;
+			}
+			else if(starr[i].length()<starr[i+1].length()&&starr[i].charAt(j)==starr[i+1].charAt(j)) {
+				if(starr[i].charAt(0)<starr[i+1].charAt(starr[i].length())) {
+					String s=starr[i];
+					starr[i]=starr[i+1];
+					starr[i+1]=s;
 				}
-
-		
 			}
-	}
+			else if(starr[i].length()>starr[i+1].length()&&starr[i].charAt(j)==starr[i+1].charAt(j)) {
+				if(starr[i].charAt(starr[i+1].length())>starr[i+1].charAt(0)) {
+					String s=starr[i];
+					starr[i]=starr[i+1];
+					starr[i+1]=s;
+				}
+			}
+			
+		}
+}
+
 		System.out.println(Arrays.toString(starr));
 
 }
