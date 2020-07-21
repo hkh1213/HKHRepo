1b234c201684fab2619b166d7b104e43fdaf254c
henry <henry_hong@naver.com>
2020-07-21 23:01:03 +0900

    cc

---
+++ SelfStudy/src/BakJoon/B1004.java
@@ -0,0 +1,38 @@
+package BakJoon;
+
+import java.util.Scanner;
+
+public class B1004 {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int i=0;
+		int count=0;
+		while(i<n) {
+			int st[]=new int[2];
+			st[0]=scn.nextInt();st[1]=scn.nextInt();
+			int end[]=new int[2];
+			end[0]=scn.nextInt();end[1]=scn.nextInt();
+			int k=scn.nextInt();
+			int arr[][]=new int[k][3];
+
+			for(int j=0;j<k;j++) {
+				for(int l=0;l<3;l++) {
+					arr[j][l]=scn.nextInt();
+					if((st[0]-arr[j][0])*(st[0]-arr[j][0])+(st[1]-arr[j][1])*(st[1]-arr[j][1])<=arr[j][2]*arr[j][2]) {
+						count+=1;
+					}
+					if((end[0]-arr[j][0])*(end[0]-arr[j][0])+(end[1]-arr[j][1])*(end[1]-arr[j][1])<=arr[j][2]*arr[j][2]) {
+						count+=1;
+					}
+
+				}
+			}
+			System.out.println(count);
+			count=0;
+			i++;
+		}
+	}
+
+}
